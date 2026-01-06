49e4047c9d004be5582629e7b32d41261fda773b
Kellyrsch <kelly_schoonderwoerd@hotmail.com>
2026-01-06 12:06:34 +0100

    testing smth for the website

--- website/main.js
+++ website/main.js
@@ -7,7 +7,7 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 
 // BRT - (Base Registry Topography) BaseMap PDOK:
 var options = { maxZoom: 14, attribution: 'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>' }
-var basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:28992/{z}/{x}/{y}.png', options);
+var basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png', options);
 
 basemap_pdok.getAttribution = function () {
   return 'BRT Achtergrondkaart <a href="http://www.kadaster.nl">Kadaster</a>.';
