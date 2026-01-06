16cdd2b15a13096d826cdc3a97cb902bb1cc10eb
Kellyrsch <kelly_schoonderwoerd@hotmail.com>
2026-01-06 16:00:15 +0100

    testing smth for the website

--- website/main.js
+++ website/main.js
@@ -3,7 +3,7 @@ let res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.88
 let map = L.map('map-canvas', {
   continuousWorld: true,
   crs: new L.Proj.CRS('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', {
-    transformation: L.Transformation(-1, -1, 0, 0),
+    //transformation: L.Transformation(-1, -1, 0, 0),
     resolutions: res,
     origin: [-285401.920, 903401.920],
     bounds: L.bounds([-285401.920, 903401.920], [595401.920, 22598.080])
